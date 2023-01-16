6f5c1260fd8f486bcda651025e30b1947a334c28
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-01-16 15:54:29 -0700

    new

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,6 @@ import Searchbar from 'components/Searchbar';
 import Button from 'components/Button';
 import Modal from 'components/Modal';
 import ImageGallery from 'components/ImageGallery';
-// import Loader from 'components/Loader';
 import { ColorRing } from 'react-loader-spinner';
 import axios from 'axios';
 import css from './app.module.css';
@@ -115,6 +114,7 @@ class App extends Component {
         )}
         {this.state.isLoading && (
           <ColorRing
+            className={css.Loader}
             visible={true}
             height="80"
             width="80"
